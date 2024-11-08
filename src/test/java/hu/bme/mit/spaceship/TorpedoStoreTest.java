86fdb797fcf76f1d67da8eca766a5e3b0bbbac60
unknown <185266@BME.HU>
2024-11-08 13:25:14 +0100

    added new unit tests

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,48 @@ class TorpedoStoreTest {
 
         // Assert
         assertEquals(true, result);
+    }
+    @Test
+    void torpedoCountTest() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(1);
+
+        // Act
+        int result = store.getTorpedoCount();
+
+        // Assert
+        assertEquals(1, result);
+    }
+    @Test
+    void torpedoIsEmptyTest() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(0);
+
+        // Act
+        boolean result = store.isEmpty();
+
+        // Assert
+        assertEquals(true, result);
+    }
+    @Test
+    void torpedoIsNotEmptyTest() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(1);
+
+        // Act
+        boolean result = store.isEmpty();
+
+        // Assert
+        assertEquals(false, result);
+    }
+    @Test
+    void fire_Failure() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(0);
+
+
+        // Assert
+        assertThrows(IllegalArgumentException.class, () ->
+        {boolean result = store.fire(1);});
     }
 }
